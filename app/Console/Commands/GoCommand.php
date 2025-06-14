79b486b0d06ffbced3779d2b1179bca15dfe224d
Anton Surygin <surygin@live.com>
2025-06-14 16:03:34 +0300

    Выполнил ДЗ

--- app/Console/Commands/GoCommand.php
+++ app/Console/Commands/GoCommand.php
@@ -2,7 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Category;
+use App\Models\Chat;
+use App\Models\Comment;
+use App\Models\Image;
+use App\Models\Message;
 use App\Models\Post;
+use App\Models\Profile;
+use App\Models\Role;
+use App\Models\Tag;
 use Illuminate\Console\Command;
 
 class GoCommand extends Command
@@ -26,9 +34,53 @@ class GoCommand extends Command
      */
     public function handle()
     {
-        Post::create([
-            'author' => 'Tony',
-            'title' => 'My first Post!',
+        Profile::create([
+            'name' => 'Tony',
+            'surname' => 'Basco',
+            'birthday_at' => '2000-10-07',
         ]);
+
+//        Chat::create([
+//            'title' => 'asd',
+//            'author' => 'Tony'
+//        ]);
+
+//        Image::create([
+//            'img_path' => 'default.jpg'
+//        ]);
+
+//        Message::create([
+//            'author' => 'Tony',
+//            'chat' => '1',
+//            'content' => 'Hello!'
+//        ]);
+
+//        Role::create(
+//            [
+//                'title' => 'admin',
+//            ],
+//            [
+//                'title' => 'user'
+//            ]
+//        );
+
+//        Tag::create([
+//            'title' => 'PHP'
+//        ]);
+//        Category::create([
+//            'title' => 'IT'
+//        ]);
+
+//        Comment::create([
+//            'author' => 'Tony',
+//            'content' => 'This is Like!',
+//            'post' => '1',
+//            'parent' => '1',
+//        ]);
+
+//        Post::create([
+//            'author' => 'Tony',
+//            'title' => 'My first Post!',
+//        ]);
     }
 }
