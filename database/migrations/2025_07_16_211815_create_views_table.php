0cba36368c3bbcdbe515112a0d00411743067bbc
Anton Surygin <surygin@live.com>
2025-07-17 14:55:31 +0300

    Доавил пару полиморфных связей

---
+++ database/migrations/2025_07_16_211815_create_views_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Profile;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+
+            $table->morphs('viewable');
+            $table->foreignIdFor(Profile::class);
+            $table->unique(['viewable_type', 'viewable_id', 'profile_id']);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('views');
+    }
+};
